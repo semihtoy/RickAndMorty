49830aef3fe4565f96ec3651de18215de25449cc
SemihTOY <semih.toy@halkbank.com.tr>
2023-06-24 20:48:11 +0300

    iOS-2 added local framework

---
+++ STCore/STCore.h
@@ -0,0 +1,18 @@
+//
+//  STCore.h
+//  STCore
+//
+//  Created by SemihTOY on 24.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STCore.
+FOUNDATION_EXPORT double STCoreVersionNumber;
+
+//! Project version string for STCore.
+FOUNDATION_EXPORT const unsigned char STCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STCore/PublicHeader.h>
+
+
---
+++ STScenes/STScenes.h
@@ -0,0 +1,18 @@
+//
+//  STScenes.h
+//  STScenes
+//
+//  Created by SemihTOY on 24.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for STScenes.
+FOUNDATION_EXPORT double STScenesVersionNumber;
+
+//! Project version string for STScenes.
+FOUNDATION_EXPORT const unsigned char STScenesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STScenes/PublicHeader.h>
+
+
