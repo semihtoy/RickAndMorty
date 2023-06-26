c5539bb08bcab01de856a9821e4caa79565b3b7f
SemihTOY <semih.toy@halkbank.com.tr>
2023-06-26 11:25:40 +0300

    iOS-4 created bundle for each local framework

---
+++ STComponents/Extensions/BundleExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  BundleExtensions.swift
+//  STComponents
+//
+//  Created by SemihTOY on 26.06.2023.
+//
+
+import Foundation
+
+public class ComponentsBundle {
+    public static var bundle: Bundle {
+        Bundle(for: Self.self)
+    }
+}
---
+++ STEntities/Extensions/BundleExtensions.swift
@@ -0,0 +1,14 @@
+//
+//  BundleExtensions.swift
+//  STEntities
+//
+//  Created by SemihTOY on 26.06.2023.
+//
+
+import Foundation
+
+public class EntitiesBundle {
+    public static var bundle: Bundle {
+        Bundle(for: Self.self)
+    }
+}
